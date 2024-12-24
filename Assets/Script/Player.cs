ad996d2a25458d543026af804433912372588eb4
MiniNija <anishkute12@gmail.com>
2024-12-25 01:04:07 +0530

    Added Physics Components and Collision Detection
    Added Circle Collider 2D components to all three spike objects to enable collision detection.
    
    Integrated a Box Collider 2D and Rigidbody 2D component to the Player game object to apply physics and enable collision interactions.
    
    Tagged the Player as "Player" and the Spikes as "Obstacles" to streamline collision detection logic.
    
    Implemented collision detection code in the Player.csscript to detect collisions between the Player and the Spikes.
    
    Created a LevelFinishTrigger to detect when the player reaches the end of the level.
    
    Added trigger detection code in the Player.csscript to handle level completion.
    
    Updated the movement code in the Player.csscript to ensure smooth and accurate player movement.

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -5,27 +5,59 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private float horizontalInput, verticalInput;
-
+    private Rigidbody2D rb;
     public float speed = 5f;
 
+    void Start() 
+    {
+        rb = GetComponent<Rigidbody2D>(); //This code finds the Rigidbody2D component on the same GameObject and stores a reference to it in the rb variable.
+    }
     private void Update()
     {
         GetInput();
         MovePlayer();
     }
+    private void FixedUpdate() => MovePlayer(); //FixedUpdate : An Event function called at a fixed time interval, independent of frame rate.
+                                                //It's perfect for physics calculations because it keeps them consistent, even if your game's frame rate varies. 
 
     private void GetInput()
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
 
-       // Debug.Log($"Horizontal: {horizontalInput}, Vertical: {verticalInput}");
+        // Debug.Log($"Horizontal: {horizontalInput}, Vertical: {verticalInput}");
     }
 
-    private void MovePlayer()
+    private void MovePlayer() //Velocity: "The speed and direction of an object's movement. In 2D games, it's represented as a Vector2 (x and y components).”
     {
-        Vector3 moveDirection = new Vector3(horizontalInput, verticalInput, 0);
-        moveDirection = moveDirection.normalized * speed * Time.deltaTime;
-        transform.position += moveDirection; 
+        Vector2 newVelocity = new Vector2(horizontalInput, verticalInput).normalized * speed;
+        rb.velocity = newVelocity; ;
     }
+    private void OnCollisionEnter2D(Collision2D other) // OnCollisionEnter2D is called when the Player game object collides with any other game object.
+    {
+        if (other.gameObject.CompareTag("Obstacle"))  // Collision2D other: This provides information about another game object that has collided with the Player game object.
+        {
+            Debug.Log(gameObject.name + " Collided with: " + other.gameObject.name);
+            //PlayerDie();             // other.gameObject.name: This gives you the name of the other object involved in the interaction.
+        }
+    }
+    private void PlayerDie()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)  // Trigger & collider.
+    {
+        if (other.gameObject.CompareTag("Finish") )    // finishing the level.
+        {
+            LevelComplete();
+        }
+    }
+    private void LevelComplete()
+    {
+        {
+            Debug.Log("Level Complete!");
+        }
+    }
+
 }
